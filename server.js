a1e0ae227217e144631a7ccede037fd3cf87b4a6
nareshb7 <nareshbjava7@gmailcom>
2025-05-08 14:46:02 +0530

    allowedOrigins added

--- server.js
+++ server.js
@@ -7,15 +7,19 @@ const routes = require("./routes");
 const PORT = 3001;
 
 const app = express();
-
+const allowedOrigins = [process.env.LOCAL_URL, process.env.LIVE_URL];
 app.use(cors());
 app.set("trust proxy", true);
 app.use(express.json({ limit: "3mb" }));
 app.use(express.urlencoded({ limit: "3mb", extended: false }));
 
 const middleware = (req, res, next) => {
-  const origin = `${req.protocol}://${req.get("host")}${req.originalUrl}`;
-  console.log("Origin URL:", origin);
+  const origin = req.get("Origin");
+  if (!allowedOrigins.includes(origin)) {
+    return res.status(403).send("Origin not allowed");
+  }
+  const url = `${origin}${req.originalUrl}`;
+  console.log("Origin URL:", url);
   next();
 };
 
